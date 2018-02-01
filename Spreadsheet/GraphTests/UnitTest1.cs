043ed7fd1647bb6606175556fedebdf4d6b71b63
Samuel <u1032555@umail.utah.edu>
2018-01-31 20:15:26 -0700

    Complete the main methods, begin testing, and start a garbage collector in the tree.

---
+++ Spreadsheet/GraphTests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Dependencies;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GraphTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAssembly()
+        {
+            DependencyGraph dg = new DependencyGraph();
+            dg.AddDependency("Cause","Effect");
+        }
+        [TestMethod]
+        public void TestForNoDuplicates()
+        {
+            DependencyGraph dg = new DependencyGraph();
+            dg.AddDependency("Cause", "Effect");
+            dg.AddDependency("Cause", "Result");
+            Assert.AreEqual(dg.Size, 3);
+        }
+    }
+}
